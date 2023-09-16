7425f5adb46a0a3e7f23421e315ef4f636ddaf1c
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-09-16 17:51:52 +0900

    :+1: パスにパラメータを含めて遷移する挙動を確認した

---
+++ pages/category/_article.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>記事：{{ this.$route.params.article }}</h2>
+  </div>
+</template>
---
+++ pages/category/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>{{ this.$route.params.article }}</h1>
+    <h2>カテゴリー</h2>
+  </div>
+</template>
