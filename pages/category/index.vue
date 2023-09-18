b2cf682e3e0d5ca27522926c094b5970251c4e29
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-09-18 14:54:02 +0900

    :+1: Vuexの値を変更できるようにした
    + ファイルを分割して役割をわけた

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <h1>{{ this.$route.params.article }}</h1>
+    <h1>{{ $route.params.article }}</h1>
     <h2>カテゴリー</h2>
+    <h3>count: {{ $store.state.count.count }}</h3>
+    <button @click="addCount">ADD</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    addCount() {
+      this.$store.dispatch('count/increment')
+    },
+  },
+}
+</script>
