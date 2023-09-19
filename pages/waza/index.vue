f153ad89b85a9d547b0b9589da496c1449d4c57b
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-09-20 00:03:31 +0900

    :+1: わざ1のデータについてターン数と充填エネルギーを表示するようにした

---
+++ pages/waza/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <select v-model="selectedItem" style="color: cornsilk">
+      <option v-for="waza in wazas" :key="waza.No" :value="waza">
+        {{ waza.わざ }}
+      </option>
+    </select>
+    <table>
+      <thead>
+        <tr>
+          <th>わざ名</th>
+          <th>タイプ</th>
+          <th>充填エネルギー</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>{{ selectedItem.わざ }}</th>
+          <th>{{ selectedItem.タイプ }}</th>
+          <th>{{ selectedItem.PvPエネルギー充填 }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import wazas from '@/assets/json/PokemonGO-waza-1.json'
+
+export default {
+  data() {
+    return {
+      wazas,
+      selectedItem: 1,
+    }
+  },
+}
+</script>
