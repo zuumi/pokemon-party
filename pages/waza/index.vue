288682ec32b93e862bb140c7b78e1049c02fc823
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-09-22 00:09:22 +0900

    :tada: わざ1にあわせてスペシャルの発動周期を計算されるようにした

--- pages/waza/index.vue
+++ pages/waza/index.vue
@@ -1,38 +1,176 @@
 <template>
   <div>
-    <select v-model="selectedItem" style="color: cornsilk">
-      <option v-for="waza in wazas" :key="waza.No" :value="waza">
-        {{ waza.わざ }}
-      </option>
-    </select>
-    <table>
-      <thead>
-        <tr>
-          <th>わざ名</th>
-          <th>タイプ</th>
-          <th>充填エネルギー</th>
-        </tr>
-      </thead>
+    <table border="3">
+      <thead></thead>
       <tbody>
-        <tr>
-          <th>{{ selectedItem.わざ }}</th>
-          <th>{{ selectedItem.タイプ }}</th>
-          <th>{{ selectedItem.PvPエネルギー充填 }}</th>
-        </tr>
+        <th>
+          <tr>
+            <label for="">ノーマル：</label>
+            <select
+              v-model="selectedItemA"
+              class="select__waza"
+              style="color: cornsilk"
+            >
+              <option v-for="wazaA in wazaAs" :key="wazaA.No" :value="wazaA">
+                {{ wazaA.わざ }}
+              </option>
+            </select>
+            <table border="1">
+              <thead>
+                <tr>
+                  <th>わざ名</th>
+                  <th>タイプ</th>
+                  <th>充填エネルギー</th>
+                  <th>ターン数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>{{ selectedItemA.わざ }}</th>
+                  <th>{{ selectedItemA.タイプ }}</th>
+                  <th>{{ selectedItemA.PvPエネルギー充填 }}</th>
+                  <th>{{ selectedItemA.ターン数 }}</th>
+                </tr>
+              </tbody>
+            </table>
+          </tr>
+          <tr>
+            <label for="">スペシャルA：</label>
+            <select
+              v-model="selectedItemB"
+              class="select__waza"
+              style="color: cornsilk"
+            >
+              <option v-for="wazaB in wazaBs" :key="wazaB.No" :value="wazaB">
+                {{ wazaB.わざ }}
+              </option>
+            </select>
+            <table border="2">
+              <thead>
+                <tr>
+                  <th>わざ名</th>
+                  <th>タイプ</th>
+                  <th>必要エネルギー</th>
+                  <th>発動周期</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>{{ selectedItemB.わざ }}</th>
+                  <th>{{ selectedItemB.タイプ }}</th>
+                  <th>{{ selectedItemB.PvPエネルギー充填 }}</th>
+                  <th>{{ enecycleA }}</th>
+                </tr>
+              </tbody>
+            </table>
+          </tr>
+          <tr>
+            <label for="">スペシャルB：</label>
+            <select
+              v-model="selectedItemC"
+              class="select__waza"
+              style="color: cornsilk"
+            >
+              <option v-for="wazaB in wazaBs" :key="wazaB.No" :value="wazaB">
+                {{ wazaB.わざ }}
+              </option>
+            </select>
+            <table border="2">
+              <thead>
+                <tr>
+                  <th>わざ名</th>
+                  <th>タイプ</th>
+                  <th>必要エネルギー</th>
+                  <th>発動周期</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>{{ selectedItemC.わざ }}</th>
+                  <th>{{ selectedItemC.タイプ }}</th>
+                  <th>{{ selectedItemC.PvPエネルギー充填 }}</th>
+                  <th>{{ enecycleB }}</th>
+                </tr>
+              </tbody>
+            </table>
+          </tr>
+        </th>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import wazas from '@/assets/json/PokemonGO-waza-1.json'
+import wazaAs from '@/assets/json/PokemonGO-waza-1.json'
+import wazaBs from '@/assets/json/PokemonGO-waza-3.json'
 
 export default {
   data() {
     return {
-      wazas,
-      selectedItem: 1,
+      wazaAs,
+      wazaBs,
+      selectedItemA: '',
+      selectedItemB: '',
+      selectedItemC: '',
     }
+  },
+  computed: {
+    enecycleA: function () {
+      return (
+        Math.ceil(
+          this.selectedItemB.PvPエネルギー充填 /
+            this.selectedItemA.PvPエネルギー充填
+        ) +
+        '-' +
+        Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 -
+            (this.selectedItemB.PvPエネルギー充填 %
+              this.selectedItemA.PvPエネルギー充填)) /
+            this.selectedItemA.PvPエネルギー充填
+        ) +
+        '-' +
+        Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 -
+            ((this.selectedItemB.PvPエネルギー充填 -
+              (this.selectedItemB.PvPエネルギー充填 %
+                this.selectedItemA.PvPエネルギー充填)) %
+              this.selectedItemA.PvPエネルギー充填)) /
+            this.selectedItemA.PvPエネルギー充填
+        )
+      )
+    },
+    enecycleB: function () {
+      return (
+        Math.ceil(
+          this.selectedItemC.PvPエネルギー充填 /
+            this.selectedItemA.PvPエネルギー充填
+        ) +
+        '-' +
+        Math.ceil(
+          (this.selectedItemC.PvPエネルギー充填 -
+            (this.selectedItemC.PvPエネルギー充填 %
+              this.selectedItemA.PvPエネルギー充填)) /
+            this.selectedItemA.PvPエネルギー充填
+        ) +
+        '-' +
+        Math.ceil(
+          (this.selectedItemC.PvPエネルギー充填 -
+            ((this.selectedItemC.PvPエネルギー充填 -
+              (this.selectedItemC.PvPエネルギー充填 %
+                this.selectedItemA.PvPエネルギー充填)) %
+              this.selectedItemA.PvPエネルギー充填)) /
+            this.selectedItemA.PvPエネルギー充填
+        )
+      )
+    },
   },
 }
 </script>
+<style lang="scss">
+.select__waza {
+  border: 2px solid #ee7917;
+}
+div {
+  text-align: left;
+}
+</style>
