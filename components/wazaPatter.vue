68d9eab537e1d5d57fe040295988af5ad6d08e8d
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-10-05 00:30:41 +0900

    :+1: 周期の計算が間違っていたので直した

--- components/wazaPatter.vue
+++ components/wazaPatter.vue
@@ -156,45 +156,49 @@ export default {
             this.selectedItemA.PvPエネルギー充填
         ) +
         '-' +
-        Math.ceil(
-          (this.selectedItemB.PvPエネルギー充填 -
-            (this.selectedItemB.PvPエネルギー充填 %
-              this.selectedItemA.PvPエネルギー充填)) /
+        (Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 * 2) /
             this.selectedItemA.PvPエネルギー充填
-        ) +
+        ) -
+          Math.ceil(
+            this.selectedItemB.PvPエネルギー充填 /
+              this.selectedItemA.PvPエネルギー充填
+          )) +
         '-' +
-        Math.ceil(
-          (this.selectedItemB.PvPエネルギー充填 -
-            ((this.selectedItemB.PvPエネルギー充填 -
-              (this.selectedItemB.PvPエネルギー充填 %
-                this.selectedItemA.PvPエネルギー充填)) %
-              this.selectedItemA.PvPエネルギー充填)) /
+        (Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 * 3) /
             this.selectedItemA.PvPエネルギー充填
-        )
+        ) -
+          Math.ceil(
+            (this.selectedItemB.PvPエネルギー充填 * 2) /
+              this.selectedItemA.PvPエネルギー充填
+          ))
       )
     },
     enecycleB: function () {
       return (
         Math.ceil(
-          this.selectedItemC.PvPエネルギー充填 /
+          this.selectedItemB.PvPエネルギー充填 /
             this.selectedItemA.PvPエネルギー充填
         ) +
         '-' +
-        Math.ceil(
-          (this.selectedItemC.PvPエネルギー充填 -
-            (this.selectedItemC.PvPエネルギー充填 %
-              this.selectedItemA.PvPエネルギー充填)) /
+        (Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 * 2) /
             this.selectedItemA.PvPエネルギー充填
-        ) +
+        ) -
+          Math.ceil(
+            this.selectedItemB.PvPエネルギー充填 /
+              this.selectedItemA.PvPエネルギー充填
+          )) +
         '-' +
-        Math.ceil(
-          (this.selectedItemC.PvPエネルギー充填 -
-            ((this.selectedItemC.PvPエネルギー充填 -
-              (this.selectedItemC.PvPエネルギー充填 %
-                this.selectedItemA.PvPエネルギー充填)) %
-              this.selectedItemA.PvPエネルギー充填)) /
+        (Math.ceil(
+          (this.selectedItemB.PvPエネルギー充填 * 3) /
             this.selectedItemA.PvPエネルギー充填
-        )
+        ) -
+          Math.ceil(
+            (this.selectedItemB.PvPエネルギー充填 * 2) /
+              this.selectedItemA.PvPエネルギー充填
+          ))
       )
     },
   },
