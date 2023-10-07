f826a900d2fba4b1371433db24e631f6504fc68f
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-10-08 00:20:59 +0900

    :shower: 不要なロゴを消した

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-    </v-col>
+    <v-col cols="12" sm="8" md="6"> </v-col>
   </v-row>
 </template>
 
