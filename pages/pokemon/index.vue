0b906857c74df5fb0e5ae4cd4f56fb11deed36f6
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-10-09 18:24:04 +0900

    :sparkles: 選択ポケモンの覚える技が同期されるようにした

--- pages/pokemon/index.vue
+++ pages/pokemon/index.vue
@@ -1,37 +1,153 @@
 <template>
-  <div>
-    <select
-      v-model="selectedPokemon"
-      class="select__pokemon"
-      @change="updateMoves"
-    >
-      <option
-        v-for="pokemon in pokemonList"
-        :key="pokemon.name"
-        :value="pokemon.name"
+  <div class="select">
+    <p>
+      <label>ポケモン：</label>
+      <select
+        v-model="selectedPokemon"
+        placeholder="ポケモンの名前を入力してください"
+        class="select__pokemon"
+        @change="updateMoves"
       >
-        {{ pokemon.name }}
-      </option>
-    </select>
-
-    <div v-if="selectedPokemon" class="select__pokemon">
-      <h2>{{ selectedPokemon.name }}</h2>
-      <p><label>技１: </label> {{ selectedPokemon.waza[0][0] }}</p>
-      <p><label>技２: </label> {{ selectedPokemon.waza[1][0] }}</p>
-      <p><label>技３: </label> {{ selectedPokemon.waza[1][1] }}</p>
+        <option
+          v-for="pokemon in pokemonList"
+          :key="pokemon.name"
+          :value="pokemon.name"
+        >
+          {{ pokemon.name }}
+        </option>
+      </select>
+    </p>
+    <div>
+      <input
+        v-model="searchTerm"
+        class="select__pokemon"
+        type="text"
+        placeholder="ポケモンの名前を入力してください"
+      />
+      <ul v-if="searchResults.length > 0">
+        <li v-for="(pokemon, index) in searchResults" :key="index">
+          {{ pokemon.name }}
+        </li>
+      </ul>
+      <p v-else>該当するポケモンは見つかりませんでした。</p>
     </div>
+    <table>
+      <thead>
+        <tr>
+          <th>わざ選択</th>
+          <th>周期</th>
+        </tr>
+      </thead>
+      <tr>
+        <th>
+          <div v-if="selectedPokemon">
+            <h2>{{ selectedPokemon.name }}</h2>
+            <p>
+              <label>技1: </label>
+              <select
+                v-model="selectedWaza1"
+                class="select__pokemon"
+                @change="setWaza1"
+              >
+                <option
+                  v-for="waza in selectedPokemon.waza[0]"
+                  :key="waza"
+                  :value="waza"
+                >
+                  {{ waza }}
+                </option>
+              </select>
+            </p>
+            <h2 v-if="selectedWaza1">{{ selectedWaza1.わざ }}</h2>
+          </div>
+        </th>
+        <th>
+          <div></div>
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <div v-if="selectedPokemon">
+            <p>
+              <label>技A: </label>
+              <select
+                v-model="selectedWazaA"
+                class="select__pokemon"
+                @change="setWazaA"
+              >
+                <option
+                  v-for="wazaA in selectedPokemon.waza[1]"
+                  :key="wazaA"
+                  :value="wazaA"
+                >
+                  {{ wazaA }}
+                </option>
+              </select>
+            </p>
+            <h2 v-if="selectedWazaA">{{ selectedWazaA.わざ }}</h2>
+          </div>
+        </th>
+        <th>
+          <div>{{ cycleA }}</div>
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <div v-if="selectedPokemon">
+            <p>
+              <label>技B: </label>
+              <select
+                v-model="selectedWazaB"
+                class="select__pokemon"
+                @change="setWazaB"
+              >
+                <option
+                  v-for="wazaB in selectedPokemon.waza[1]"
+                  :key="wazaB"
+                  :value="wazaB"
+                >
+                  {{ wazaB }}
+                </option>
+              </select>
+            </p>
+            <h2 v-if="selectedWazaB">{{ selectedWazaB.わざ }}</h2>
+          </div>
+        </th>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 import pokemonData from '@/assets/json/pokemon_data.json'
+import waza1Data from '@/assets/json/pokemon_data_waza_1.json'
+import wazaABData from '@/assets/json/pokemon_data_waza_3.json'
 
 export default {
   data() {
     return {
-      pokemonList: Object.values(pokemonData['pokemon_data.json']),
+      searchTerm: '',
+      pokemonList: Object.entries(pokemonData['pokemon_data.json']).map(
+        ([, value]) => value
+      ),
+      waza1List: Object.values(waza1Data),
+      wazaABList: Object.values(wazaABData),
       selectedPokemon: null,
+      selectedWaza1: null,
+      selectedWazaA: null,
+      selectedWazaB: null,
     }
+  },
+  computed: {
+    searchResults() {
+      if (this.searchTerm === '') {
+        return []
+      }
+      const searchTermLower = this.searchTerm.toLowerCase()
+      return this.pokemonList.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchTermLower)
+      )
+    },
   },
   methods: {
     updateMoves() {
@@ -40,12 +156,36 @@ export default {
       )
       this.selectedPokemon = selectedPokemon
     },
+    setWaza1() {
+      const selectedWaza1 = this.waza1List.find(
+        (waza1) => waza1.わざ === this.selectedWaza1
+      )
+      this.selectedWaza1 = selectedWaza1
+    },
+    setWazaA() {
+      const selectedWazaA = this.wazaABList.find(
+        (wazaA) => wazaA.わざ === this.selectedWazaA
+      )
+      this.selectedWazaA = selectedWazaA
+    },
+    setWazaB() {
+      const selectedWazaB = this.wazaABList.find(
+        (wazaB) => wazaB.わざ === this.selectedWazaB
+      )
+      this.selectedWazaB = selectedWazaB
+    },
   },
 }
 </script>
 <style lang="scss">
+.select {
+  border: 2px solid #ee7917;
+}
 .select__pokemon {
   border: 2px solid #ee7917;
+  color: white;
+  background-color: #333;
+  width: 250px;
 }
 div {
   text-align: left;
