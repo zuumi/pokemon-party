b8e9d5bbcf810c987cfcc3db7388659b0e22e523
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-10-11 00:58:19 +0900

    :+1: 子コンポーネントにデータを渡してみた

--- pages/waza/index.vue
+++ pages/waza/index.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="parent">
-    <waza-patter></waza-patter>
-    <waza-patter></waza-patter>
-    <waza-patter></waza-patter>
+    <waza-patter :selected1="waza1"></waza-patter>
+    <waza-patter :selected1="waza2"></waza-patter>
+    <waza-patter :selected1="waza3"></waza-patter>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 import wazaPatter from '~/components/wazaPatter.vue'
 
 export default Vue.extend({
   components: {
     wazaPatter,
+  },
+  data() {
+    return {
+      waza1: 'カウンター',
+      waza2: 'カウンター',
+      waza3: 'カウンター',
+    }
   },
 })
 </script>
